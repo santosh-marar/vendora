4dfcf0f390825e6e8450c5005b48eead72e80999
santosh-marar <st.juniordeveloper@gmail.com>
2026-01-29 23:21:16 +0545

    chore: update all depency

--- proxy.ts
+++ proxy.ts
@@ -113,7 +113,7 @@ const isRouteAccessibleToRole = (
 };
 
 // Middleware function
-export default async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   const { nextUrl } = req;
   const path = nextUrl.pathname;
 
@@ -132,7 +132,7 @@ export default async function middleware(req: NextRequest) {
 
   // Get authentication status and roles from the session
   const isAuthenticated = !!session?.user;
-const userRoles = (session?.user?.role as string[]) || [];
+  const userRoles = (session?.user?.role as string[]) || [];
 
   // Check if the route is accessible
   const canAccess = isRouteAccessibleToRole(path, userRoles, isAuthenticated);
